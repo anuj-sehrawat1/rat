483af98ef04ac4542635bd9294c859e7b4a82635
Anuj Sehrawat <143939648+anuj-sehrawat1@users.noreply.github.com>
2025-12-03 17:18:05 +0530

    Refactor DATABASE_URL variable to DB

--- api/full.js
+++ api/full.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   try {
-    const DATABASE_URL = process.env.DATABASE_URL;
-    const response = await axios.get(`${DATABASE_URL}/.json`);
+    const DB = process.env.DATABASE_URL;
+    const response = await axios.get(`${DB}/.json`);
     res.status(200).json(response.data);
   } catch (err) {
     res.status(500).json({ error: err.message });
